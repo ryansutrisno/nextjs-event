086517bed63db59bb05eb7a76e8a3481da76fa2a
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2023-02-23 18:25:38 +0700

    added input comment and newsletter, setting api routes & mongodb

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,12 +1,23 @@
+import {useRef} from 'react';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
+  const emailInputRef = useRef();
+
   function registrationHandler(event) {
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    const enteredEmail = emailInputRef.current.value;
+
+    fetch('/api/newsletter', {
+      method: 'POST',
+      body: JSON.stringify({email: enteredEmail}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
@@ -15,6 +26,7 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
+            ref={emailInputRef}
             type="email"
             id="email"
             placeholder="Your email"
---
+++ pages/api/newsletter.js
@@ -0,0 +1,22 @@
+import {MongoClient} from 'mongodb';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const userEmail = req.body.email;
+
+    if (!userEmail || !userEmail.includes('@')) {
+      res.status(422).json({message: 'Invalid email address.'});
+      return;
+    }
+
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
+      const db = client.db('newsletter');
+
+      await db.collection('emails').insertOne({email: userEmail});
+
+      client.close();
+
+    res.status(201).json({message: 'Signed up!'});
+  }
+}
+export default handler;
